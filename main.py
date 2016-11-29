35732e51264c31c00dda72157fe7e7499e8077c7
Thanh Cong Le <thanhlct@gmail.com>
2016-11-29 05:52:19 +0100

    RNN with mini batch training, still very slow

--- main.py
+++ main.py
@@ -83,7 +83,11 @@ valid_percent = 0.2
 test_percent = 0.2
 token_type = 'word'
 vocabs, word2id, id2word, vocab_size = None, None, None, None
-max_seq_len = 350#TODO:change later when uses full document words
+
+max_seq_len = 200#TODO:change later when uses full document words
+lstm_hidden_size = 100
+class_size = 60
+batch_size = 500
 
 train_full = read_corpus(data_dir, 0, train_percent)
 test_full = read_corpus(data_dir, train_percent, 1.0)
@@ -139,11 +143,6 @@ def score(sess, predict_op, input_vecs, test_docs):
     return correct/float(total) 
 
 def run1():
-    lstm_hidden_size = 100
-    class_size = 60
-    batch_size = 10
-    #max_seq_len = 500 #TODO change to dynamic sequence length
-
     build_vocab()
 
     with tf.Graph().as_default():
@@ -181,6 +180,7 @@ def run1():
 
         #define train
         optimizer = tf.train.AdamOptimizer()
+        #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
         train = optimizer.minimize(cross_entropy)
 
         '''
@@ -214,7 +214,8 @@ def run1():
     #                batch_outputs.append(y)
                     ret = sess.run([train, loss], feed_dict={input_vecs:vecs, target:y})
                     #print('doc', i)
-                print('score pas======', pas, ':', score(sess, prediction, input_vecs, test_data))
+                print('score in train, pas======', pas, ':', score(sess, prediction, input_vecs, train_data))
+                print('score in test, pas======', pas, ':', score(sess, prediction, input_vecs, test_data))
                 
                 '''
                 if count==batch_size:
@@ -224,25 +225,118 @@ def run1():
                     batch_inputs = []
                     batch_outputs = []
                 '''
-
+def score2(sess, predict_op, input_vecs, test_docs):
+    total = 0
+    correct = 0
+    count = 0
+    batch_inputs = []
+    batch_outputs = []
+    for doc in test_docs:
+        vecs = get_input_words_vec(doc.words)
+        batch_inputs.append(vecs)
+        batch_outputs.append(doc.topic_id)
+        total +=1
+        count +=1
+        if count==batch_size:
+            rets = sess.run([predict_op], feed_dict={input_vecs:batch_inputs, })
+            rets = rets[0]
+            preds = np.argmax(rets, axis=1)
+            num_correct = len(np.where(np.equal(preds, batch_outputs)==True)[0])
+            correct += num_correct 
+            count = 0
+            batch_inputs = []
+            batch_outputs = []
+    return correct/float(total) 
 
 def run2():
-    lstm_size = 100
-    class_size = 60
+    Log.info(None, '---------RUN 2-------')
+    #max_seq_len = 500 #TODO change to dynamic sequence length
 
     build_vocab()
 
-    cell = tf.nn.rnn_cell.LSTMCell(lstm_size)
-    for doc in train_data:
-        vecs = get_input_words_vec(doc.words)
-        init = cell.zero_state(lstm_size, tf.float32)
-        #lst_vec = vecs.tolist()
-        pdb.set_trace()
-        #outputs, last_state = tf.nn.rnn(cell, lst_vec)
+    with tf.Graph().as_default():
+        Log.info(None, 'Structure the graph...')
+        #define the model
+        #input_vecs = tf.placeholder(tf.int32, shape=(None, vocab_size))
+        #target = tf.placeholder(tf.int32, shape=(batch_size, class_size))
+        #input_vecs = tf.placeholder(tf.float32, shape=(max_seq_len, vocab_size))
+        #target = tf.placeholder(tf.float32, shape=(1, class_size))
+
+        input_vecs = tf.placeholder(tf.float32, shape=(batch_size, max_seq_len, vocab_size))
+        targets = tf.placeholder(tf.float32, shape=(batch_size, class_size))
+        #inputs = tf.unpack(input_vecs, 500, axis=0)#equivalent to the line below
+        #inputs = tf.split(0, max_seq_len, input_vecs)# each row is an input
+        #inputs = tf.transpose(input_vecs) 
+        #inputs = tf.split(1, 500, inputs)
+
+        cell = tf.nn.rnn_cell.LSTMCell(lstm_hidden_size)
+        #init_state = cell.zero_state(1, dtype=tf.float32)
+        #outputs, last_state = tf.nn.rnn(cell, inputs, dtype=tf.float32)
+        outputs, last_state = tf.nn.dynamic_rnn(cell, input_vecs, dtype=tf.float32)
+
+        #val = tf.transpose(outputs, [1, 0, 2])
+        #last = tf.gather(val, int(val.get_shape()[0]) - 1)
+        last_outputs = outputs[:, -1, :]
+
+        #define predict, softmax
+        weight = tf.Variable(tf.truncated_normal([lstm_hidden_size, int(targets.get_shape()[1])]))
+        bias = tf.Variable(tf.constant(0.1, shape=[targets.get_shape()[1]]))
+        predictions = tf.nn.softmax(tf.matmul(last_outputs, weight) + bias) 
+
+        #define loss
+        cross_entropy = -tf.reduce_sum(targets * tf.log(predictions))
+        loss = cross_entropy
+
+        #define train
+        optimizer = tf.train.AdamOptimizer()
+        #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+        train = optimizer.minimize(cross_entropy)
+
+        '''
+        #define loss
+        target_one_hot = tf.one_hot(target, class_size, 1, 0)
+        logits = tf.contrib.layers.fully_connected(last_state, class_size, activation_fn=None)
+        loss = tf.contrib.losses.softmax_cross_entropy(logits, target)
+
+        #define train op
+        train = tf.contrib.layers.optimize_loss(loss, tf.contrib.framework.get_global_step(), optimizer='Adam', learning_rate=0.01)
+        #optimizer = tf.train.GradientDescentOptimizer(0.01)
+        #train = optimizer.minimize(loss) 
+        '''
+        #initilization
+        init = tf.initialize_all_variables()
+        count = 0
+        batch_inputs = []
+        batch_outputs = []
+
+        with tf.Session() as sess:
+            sess.run(init)
+            print('score at init...')
+            print('score in train at init:', score2(sess, predictions, input_vecs, train_data))
+            for pas in range(10):
+                print('train======pas', pas)
+                for i, doc in enumerate(train_data):
+                    count +=1
+                    vecs = get_input_words_vec(doc.words)
+                    batch_inputs.append(vecs)
+                    #y = np.zeros(shape=(class_size, ))
+                    y = [0]*class_size
+                    y[doc.topic_id] = 1
+                    batch_outputs.append(y)
+                    #ret = sess.run([train, loss], feed_dict={input_vecs:vecs, target:y})
+                
+                    if count==batch_size:
+                        print('-------------batch', (i+1)/batch_size)
+                        sess.run([train, loss], feed_dict={input_vecs:batch_inputs, targets:batch_outputs})
+                        count = 0
+                        batch_inputs = []
+                        batch_outputs = []
+                print('score in train, pas======', pas, ':', score2(sess, predictions, input_vecs, train_data))
+                print('score in test, pas======', pas, ':', score2(sess, predictions, input_vecs, test_data))
 
 def main():
-    run1()
-    #run2()
+    #run1()
+    run2()
     print('DONE')
 
 if __name__ == '__main__':
